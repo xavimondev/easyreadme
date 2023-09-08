7733341c55489402da7b91e6cac5ad2ab8a02d30
Xavier Alfaro <nxb.developer@gmail.com>
2023-09-08 18:51:36 -0500

    add next themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
-import './globals.css'
+import '../styles/globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ThemeProvider } from '@/components/theme-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,7 +13,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider attribute='class' defaultTheme='dark' enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
