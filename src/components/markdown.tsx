4a369b9b7fa5f3c3b3eeb27e37e2b1e8a86cdd21
Xavier Alfaro <nxb.developer@gmail.com>
2023-09-10 21:49:00 -0500

    add tabs to display code and editor

--- src/components/markdown.tsx
+++ src/components/markdown.tsx
@@ -1,8 +1,9 @@
 'use client'
 import { useEffect, useState } from 'react'
-import { marked } from 'marked'
 import { copyToClipboard } from '@/utils'
 import { CheckIc, CopyIc } from '@/components/icons'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { CustomEditor } from '@/components/editor'
 
 const mkd = `## Features
 
@@ -26,7 +27,6 @@ Although it has its roots in Catholicism, Advent has become a tradition where ch
 `
 export function Markdown() {
   const [isCopied, setIsCopied] = useState(false)
-  const html = marked(mkd)
 
   useEffect(() => {
     let timeout: ReturnType<typeof setTimeout> | null = null
@@ -39,29 +39,34 @@ export function Markdown() {
     }
   }, [isCopied])
   return (
-    <div className='flex flex-col md:flex-row gap-4 w-full'>
-      <div className='w-full rounded-md overflow-hidden relative'>
-        <textarea
-          className='border border-black dark:border-white/20 rounded-md text-black dark:text-white resize-none w-full h-full outline-none p-5 bg-white/95 dark:bg-white/5'
-          readOnly
-          defaultValue={mkd}
-        ></textarea>
-        <button
-          className='absolute right-4 top-4 rounded-md p-1.5 hover:bg-white/10 transition-colors duration-200'
-          onClick={async () => {
-            setIsCopied(!isCopied)
-            await copyToClipboard(mkd)
-          }}
-        >
-          {isCopied ? <CheckIc className='w-4 h-4' /> : <CopyIc className='w-4 h-4' />}
-        </button>
-      </div>
-      <div className='border border-black dark:border-white/20 w-full rounded-md p-5 bg-white/95 dark:bg-white/5'>
-        <article
-          className='w-full prose prose-sm prose-neutral dark:prose-invert max-w-none'
-          dangerouslySetInnerHTML={{ __html: html }}
-        ></article>
-      </div>
-    </div>
+    <Tabs defaultValue='editor' className='w-full'>
+      <TabsList className='grid w-56 grid-cols-2 h-9'>
+        <TabsTrigger value='editor'>Editor</TabsTrigger>
+        <TabsTrigger value='code'>Code</TabsTrigger>
+      </TabsList>
+      <TabsContent value='editor'>
+        <div className='border border-black dark:border-white/20 w-full rounded-md p-5 bg-white/95 dark:bg-white/5 relative h-[calc(100vh-78px)]'>
+          <button
+            className='absolute right-4 top-4 rounded-md p-1.5 hover:bg-white/10 transition-colors duration-200'
+            onClick={async () => {
+              setIsCopied(!isCopied)
+              await copyToClipboard(mkd)
+            }}
+          >
+            {isCopied ? <CheckIc className='w-5 h-5' /> : <CopyIc className='w-5 h-5' />}
+          </button>
+          <CustomEditor content={mkd} />
+        </div>
+      </TabsContent>
+      <TabsContent value='code'>
+        <div className='w-full rounded-md overflow-hidden h-[calc(100vh-78px)]'>
+          <textarea
+            className='border border-black dark:border-white/20 rounded-md text-black dark:text-white resize-none w-full h-full outline-none p-5 bg-white/95 dark:bg-white/5 text-base sm:text-lg'
+            readOnly
+            defaultValue={mkd}
+          ></textarea>
+        </div>
+      </TabsContent>
+    </Tabs>
   )
 }
