f63d0329f13c971e645230a3f872d1a41590193a
Xavier Alfaro <nxb.developer@gmail.com>
2023-09-10 21:35:01 -0500

    update editor's ui

--- src/components/editor/index.tsx
+++ src/components/editor/index.tsx
@@ -5,12 +5,11 @@ import { DEFAULT_EXTENSIONS } from './extensiones'
 export function CustomEditor({ content }: { content: string }) {
   const editor = useEditor({
     injectCSS: false,
-    autofocus: 'end',
     content,
     editorProps: {
       attributes: {
         class:
-          'prose prose-sm sm:prose-base prose-neutral dark:prose-invert max-w-none font-default focus:outline-none h-[calc(100vh-80px)] overflow-y-auto scrollbar-hide'
+          'prose prose-sm sm:prose-base prose-neutral dark:prose-invert max-w-none font-default focus:outline-none h-[calc(100vh-120px)] overflow-y-auto scrollbar-hide'
       }
     },
     extensions: [...DEFAULT_EXTENSIONS],
