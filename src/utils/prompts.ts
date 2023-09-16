4c44b5b16bebefd353cbf770204336417f8854ad
Xavier Alfaro <nxb.developer@gmail.com>
2023-09-16 12:58:20 -0500

    update prompts

--- src/utils/prompts.ts
+++ src/utils/prompts.ts
@@ -7,11 +7,12 @@ export const getPromptOverview = ({
   directoryTree: string
 }) => {
   return `You are a Senior Software Engineer.
-  Your goal is to generate an overview of the given project using its name ${repositoryName} and its directory tree:
-  ${directoryTree}
-  Provide a precise overview that captures the fundamental features and project's goal.
-  Keep your response under 300 characters (including spaces).
-  Return as a paragraph.`
+Your goal is to generate an overview of the given project using its name ${repositoryName} and its directory tree:
+
+${directoryTree}
+Provide a precise overview that captures the fundamental features and project's goal.
+Keep your response under 300 characters (including spaces).
+Return as a paragraph.`
 }
 
 export const generateTechStack = ({
@@ -22,7 +23,8 @@ export const generateTechStack = ({
   language: string
 }) => {
   return `Given the following ${language} project dependencies:
-  ${dependencies}
+  
+${dependencies}
 
 Pick the 10 most important dependencies that cover the following aspects: authentication, data fetching, state management, 
 animations, styling, server-client communication, testing and deployment. 
