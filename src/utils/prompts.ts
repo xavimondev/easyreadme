9a4e915879650f4897481575cc9392b8b533c352
Xavier Alfaro <nxb.developer@gmail.com>
2023-09-18 11:14:31 -0500

    update prompt

--- src/utils/prompts.ts
+++ src/utils/prompts.ts
@@ -6,13 +6,12 @@ export const getPromptOverview = ({
   repositoryName: string
   directoryTree: string
 }) => {
-  return `You are a Senior Software Engineer.
-Your goal is to generate an overview of the given project using its name ${repositoryName} and its directory tree:
+  return `Generate an overview of the given project using its name ${repositoryName} and its directory tree:
 
 ${directoryTree}
 Provide a precise overview that captures the fundamental features and project's goal.
-Keep your response under 300 characters (including spaces).
-Return as a paragraph.`
+Keep your response under 200 characters (including spaces).
+Only respond with a markdown.`
 }
 
 export const generateTechStack = ({
@@ -23,15 +22,14 @@ export const generateTechStack = ({
   language: string
 }) => {
   return `Given the following ${language} project dependencies:
-  
 ${dependencies}
 
 Pick the 10 most important dependencies that cover the following aspects: authentication, data fetching, state management, 
 animations, styling, server-client communication, testing and deployment. 
 Generate a summary like this:
-- [name_dependency][link_resource]: Short explanation about the dependency.  Limit your response to a maximum of 100 characters.
+- [name_dependency][link_resource]: Short explanation about the dependency.Limit your response to a maximum of 100 characters.
 Where link_resource is dependency's url information.
-Return your response as a markdown using bullet points.`
+Only respond with a markdown.`
 }
 
 export const generateGuideEnvironmentVariables = ({
@@ -40,14 +38,11 @@ export const generateGuideEnvironmentVariables = ({
   environmentVars: string
 }) => {
   return `Given the following environment variables:
-
 ${environmentVars}
 
-Please provide a precise guide to generate their values.
-Each guide has a header and a enumerated list. Example:
-## Supabase
-Generated list
-
-Generate your response as a Markdown enumerated list.
-Limit your response to a maximum of 1000 characters.`
+Please provide a brief and precise guide to generate their values.
+Each guide should have a header and a enumerated list.
+Limit your response to a maximum of 500 characters.
+Only responde with a markdown.
+`
 }
