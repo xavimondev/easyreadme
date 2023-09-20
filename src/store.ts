698a8a15b9c8211fae21bd1454ca21e3383f04e6
Xavier Alfaro <nxb.developer@gmail.com>
2023-09-19 22:23:31 -0500

    add new state to handle generating status

--- src/store.ts
+++ src/store.ts
@@ -5,12 +5,16 @@ type TemplateState = {
   setTemplateSelected: (templateName: string) => void
   contentTemplate: string
   setContentTemplate: (content: string) => void
+  isGenerating: boolean
+  setIsGenerating: (isGenerating: boolean) => void
 }
 
 export const useTemplate = create<TemplateState>()((set) => ({
   templateSelected: undefined,
   contentTemplate: '',
+  isGenerating: false,
   setTemplateSelected: (templateName) => set({ templateSelected: templateName }),
   setContentTemplate: (content: string) =>
-    set((prevContent) => ({ contentTemplate: prevContent.contentTemplate.concat(content) }))
+    set((prevContent) => ({ contentTemplate: prevContent.contentTemplate.concat(content) })),
+  setIsGenerating: (isGenerating) => set({ isGenerating })
 }))
