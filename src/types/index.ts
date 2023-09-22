ad0cb808093c46d68dc55da45c5e18a4bfeccdc8
Xavier Alfaro <nxb.developer@gmail.com>
2023-09-22 17:43:00 -0500

    update template types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,11 @@
+export type NameTemplate = 'Minimal' | 'Collaborate' | 'Inspire' | 'Empower' | 'Unleash'
+
 export type Template = {
   srcImage: string
   altImage: string
-  nameTemplate: string
-  authorTemplate: string
-  urlAuthor: string
+  nameTemplate: NameTemplate
+  authorTemplate?: string
+  urlAuthor?: string
 }
 
 export type GitTreeResponse = {
