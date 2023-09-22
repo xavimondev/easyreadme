c45cb00f9153d79f34b0a9e85d40c73a91dca6d7
Xavier Alfaro <nxb.developer@gmail.com>
2023-09-22 17:47:29 -0500

    add templates

--- src/hooks/use-templates.tsx
+++ src/hooks/use-templates.tsx
@@ -25,17 +25,78 @@ export function useTemplates() {
   } = useTemplateSections()
   const setIsGenerating = useTemplate((state) => state.setIsGenerating)
 
-  const minimalTemplate = async ({ promptBuilder }: { promptBuilder: PromptBuilder }) => {
+  const minimal = async ({ promptBuilder }: { promptBuilder: PromptBuilder }) => {
+    setIsGenerating(true)
+    await overview({ promptBuilder })
+    await settingUp({ promptBuilder })
+    await runningLocally({ promptBuilder })
+    deploy({ promptBuilder })
+    await license({ promptBuilder })
+    setIsGenerating(false)
+  }
+
+  const collaborate = async ({ promptBuilder }: { promptBuilder: PromptBuilder }) => {
     setIsGenerating(true)
     banner({ promptBuilder })
+    badges({ promptBuilder })
+    await techStack({ promptBuilder })
+    await projectSummary({ promptBuilder })
+    await settingUp({ promptBuilder })
+    await runningLocally({ promptBuilder })
+    await tableContributors({ promptBuilder })
+    await faq({ promptBuilder })
+    await license({ promptBuilder })
+    setIsGenerating(false)
+  }
+
+  const inspire = async ({ promptBuilder }: { promptBuilder: PromptBuilder }) => {
+    setIsGenerating(true)
+    banner({ promptBuilder })
+    badges({ promptBuilder })
+    await overview({ promptBuilder })
+    await projectStructure({ promptBuilder })
+    await projectSummary({ promptBuilder })
+    await techStack({ promptBuilder })
+    await settingUp({ promptBuilder })
+    await runningLocally({ promptBuilder })
+    galleryContributors({ promptBuilder })
+    deploy({ promptBuilder })
+    await license({ promptBuilder })
+    setIsGenerating(false)
+  }
+
+  const empower = async ({ promptBuilder }: { promptBuilder: PromptBuilder }) => {
+    setIsGenerating(true)
     await overview({ promptBuilder })
     await techStack({ promptBuilder })
     await settingUp({ promptBuilder })
     await runningLocally({ promptBuilder })
+    await roadmap({ promptBuilder })
+    acknowledgments({ promptBuilder })
+    await changelog({ promptBuilder })
+    setIsGenerating(false)
+  }
+
+  const unleash = async ({ promptBuilder }: { promptBuilder: PromptBuilder }) => {
+    setIsGenerating(true)
+    banner({ promptBuilder })
+    badges({ promptBuilder })
+    await overview({ promptBuilder })
+    await projectStructure({ promptBuilder })
+    prerequisites({ promptBuilder })
+    await runningLocally({ promptBuilder })
+    await faq({ promptBuilder })
+    await roadmap({ promptBuilder })
+    acknowledgments({ promptBuilder })
+    await license({ promptBuilder })
     setIsGenerating(false)
   }
 
   return {
-    minimalTemplate
+    minimal,
+    collaborate,
+    inspire,
+    empower,
+    unleash
   }
 }
