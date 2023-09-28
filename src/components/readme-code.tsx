94cf40e455b73428c10e99e987be0d4fe8642d60
Xavier Alfaro <nxb.developer@gmail.com>
2023-09-27 23:23:17 -0500

    add syntax highlighting to markdown code block

--- src/components/readme-code.tsx
+++ src/components/readme-code.tsx
@@ -1,13 +1,43 @@
+'use client'
+import { useEffect } from 'react'
+import { useTheme } from 'next-themes'
+import Lowlight from 'react-lowlight'
+import markdown from 'highlight.js/lib/languages/markdown'
+
 type ReadmeCodeProps = {
   content: string
 }
 
+Lowlight.registerLanguage('markdown', markdown)
+
 export function ReadmeCode({ content }: ReadmeCodeProps) {
+  const { theme } = useTheme()
+
+  useEffect(() => {
+    const loadTheme = async () => {
+      await import(`highlight.js/styles/base16/default-${theme}.css`)
+    }
+    content && loadTheme()
+  }, [theme])
+
   return (
-    <textarea
-      className='border border-black dark:border-white/20 rounded-md text-black dark:text-white resize-none w-full h-full outline-none p-5 bg-white/95 dark:bg-white/5 text-base sm:text-lg scrollbar-hide'
-      readOnly
+    <Lowlight
+      language='markdown'
       value={content}
-    ></textarea>
+      className='border 
+        border-black 
+        dark:border-white/20 
+        rounded-md 
+        w-full 
+        h-full 
+        outline-none 
+        text-base
+        sm:text-lg 
+        overflow-hidden 
+        [&>code]:h-full 
+        [&>code]:w-full 
+        [&>code]:p-3
+        [&>code]:scrollbar-hide'
+    />
   )
 }
