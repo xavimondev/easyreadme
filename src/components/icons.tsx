79d7cbbbd27b719884ddc5d912f81ae8a32f2797
Xavier Alfaro <nxb.developer@gmail.com>
2023-09-29 18:55:18 -0500

    add icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -104,3 +104,18 @@ export function ColumnsIc(props: SVGProps<SVGSVGElement>) {
     </svg>
   )
 }
+
+export function SlideShowIc(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='32'
+      height='32'
+      fill='currentColor'
+      viewBox='0 0 256 256'
+      {...props}
+    >
+      <path d='M192,48H64A16,16,0,0,0,48,64V192a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64A16,16,0,0,0,192,48Zm0,144H64V64H192V192ZM240,56V200a8,8,0,0,1-16,0V56a8,8,0,0,1,16,0ZM32,56V200a8,8,0,0,1-16,0V56a8,8,0,0,1,16,0Z'></path>
+    </svg>
+  )
+}
