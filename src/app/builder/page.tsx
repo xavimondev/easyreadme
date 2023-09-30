a00c2e5e60d80cc64207f345e720d95f3f0f5958
Xavier Alfaro <nxb.developer@gmail.com>
2023-09-29 20:06:17 -0500

    add responsive

--- src/app/builder/page.tsx
+++ src/app/builder/page.tsx
@@ -4,7 +4,7 @@ import { Sidebar } from '@/components/sidebar'
 export default function Builder() {
   return (
     <main className='p-2 min-h-screen'>
-      <div className='h-full w-full grid grid-cols-[300px,_1fr] gap-3'>
+      <div className='h-full w-full grid grid-cols-1 md:grid-cols-[300px,_1fr] gap-3'>
         <Sidebar />
         <ReadmeBuilder />
       </div>
