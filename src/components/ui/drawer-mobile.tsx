58168a668b1291ac35a62c297667506c9e69669e
Xavier Alfaro <nxb.developer@gmail.com>
2023-10-05 23:30:37 -0500

    add dark styles

--- src/components/ui/drawer-mobile.tsx
+++ src/components/ui/drawer-mobile.tsx
@@ -22,8 +22,8 @@ export function DrawerMobile({
     <Drawer.Root open={open} onOpenChange={setOpen}>
       <Drawer.Trigger asChild>{btnOpen}</Drawer.Trigger>
       <Drawer.Portal>
-        <Drawer.Overlay className='fixed inset-0 bg-background/70 backdrop-blur-sm' />
-        <Drawer.Content className='bg-white/95 dark:bg-[#161518] flex flex-col rounded-t-[10px] h-[95%] mt-24 fixed bottom-0 left-0 right-0'>
+        <Drawer.Overlay className='fixed inset-0 bg-black/60 dark:bg-background/70 backdrop-blur-sm' />
+        <Drawer.Content className='bg-white dark:bg-[#161518] flex flex-col rounded-t-[10px] h-[95%] mt-24 fixed bottom-0 left-0 right-0'>
           <div className='p-4 rounded-t-[10px] flex-1'>
             <div className='mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300 mb-6' />
             <div className='max-w-md mx-auto h-full'>
