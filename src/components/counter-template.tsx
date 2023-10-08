105585ee4d6278c81bd407ac60228bea461f943f
Xavier Alfaro <nxb.developer@gmail.com>
2023-10-08 12:47:37 -0500

    update color

--- src/components/counter-template.tsx
+++ src/components/counter-template.tsx
@@ -7,7 +7,7 @@ export function CounterTemplate() {
   const { data, isLoading } = useRemaining()
 
   return (
-    <div className='flex rounded-md bg-sky-400 dark:bg-sky-900/30 px-3 py-1'>
+    <div className='flex rounded-md bg-sky-200 dark:bg-sky-900/30 px-3 py-1'>
       <p className='text-sm text-sky-900 dark:text-sky-500 font-semibold flex items-center'>
         {isLoading ? <LoadingIc className='w-3 h-3 mr-2 animate-twSpin' /> : `${data.remaining}`}
         {` / ${RATE_LIMIT} templates`}
