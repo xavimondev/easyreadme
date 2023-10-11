3ae0580a00f393080233b13e5e2ffe10f0d68305
Xavier Alfaro <nxb.developer@gmail.com>
2023-10-11 12:25:40 -0500

    fix an extra line break is added in the last command

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -1,8 +1,9 @@
 export const getSetupCommands = ({ commands }: { commands: string[] }) => {
   let setup = `\`\`\`bash\n`
 
-  commands.forEach((command) => {
-    setup += `\n${command}`.trim() + `\n\n`
+  commands.forEach((command, index) => {
+    const breakLine = index === commands.length - 1 ? `\n` : `\n\n`
+    setup += `\n${command}`.trim() + `${breakLine}`
   })
   setup += `\`\`\``
   return setup
