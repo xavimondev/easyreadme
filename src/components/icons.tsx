129bb71bb459e271967d6d5eff6be8af5751e26d
Xavi Alfaro <nxb.developer@gmail.com>
2024-01-04 15:10:40 -0500

    add new icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -245,3 +245,18 @@ export function CopyWithGradientIc(props: SVGProps<SVGSVGElement>) {
     </svg>
   )
 }
+
+export function PlusIc(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='32'
+      height='32'
+      fill='currentColor'
+      viewBox='0 0 256 256'
+      {...props}
+    >
+      <path d='M224 128a8 8 0 0 1-8 8h-80v80a8 8 0 0 1-16 0v-80H40a8 8 0 0 1 0-16h80V40a8 8 0 0 1 16 0v80h80a8 8 0 0 1 8 8Z' />
+    </svg>
+  )
+}
