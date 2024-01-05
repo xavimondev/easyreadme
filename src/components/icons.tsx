4d1dc802b0e3cd067ca5ca238c6d7e37bde30718
Xavi Alfaro <nxb.developer@gmail.com>
2024-01-04 23:20:36 -0500

    add remove icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -260,3 +260,18 @@ export function PlusIc(props: SVGProps<SVGSVGElement>) {
     </svg>
   )
 }
+
+export function RemoveIc(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='32'
+      height='32'
+      fill='currentColor'
+      viewBox='0 0 256 256'
+      {...props}
+    >
+      <path d='M216 48H40a8 8 0 0 0 0 16h8v144a16 16 0 0 0 16 16h128a16 16 0 0 0 16-16V64h8a8 8 0 0 0 0-16Zm-24 160H64V64h128ZM80 24a8 8 0 0 1 8-8h80a8 8 0 0 1 0 16H88a8 8 0 0 1-8-8Z' />
+    </svg>
+  )
+}
