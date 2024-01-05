8b621c444d5e6ba44c61e7f8ace57464dba0d4d9
Xavi Alfaro <nxb.developer@gmail.com>
2024-01-05 14:43:51 -0500

    fix div element inside p element

--- src/components/editor/components/banner.tsx
+++ src/components/editor/components/banner.tsx
@@ -3,7 +3,7 @@ import { ActionsBar } from './actions-bar'
 
 export function Banner({ deleteNode }: any) {
   return (
-    <NodeViewWrapper className='!m-0 !p-0' as='p'>
+    <NodeViewWrapper className='!m-0 !p-0' as='div'>
       <div className='relative group'>
         <a href='https://github.com/xavimondev/supaplay' target='_blank'>
           <img src='/placeholder.jpg' className='!m-0' width='100%' alt='Banner' />
