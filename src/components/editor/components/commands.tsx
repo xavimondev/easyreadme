e29f6ad656b16ecd63611ac7947b129fb6a4d059
Xavi Alfaro <nxb.developer@gmail.com>
2024-01-09 18:53:32 -0500

    add commands tiptap component

---
+++ src/components/editor/components/commands.tsx
@@ -0,0 +1,29 @@
+import { NodeViewWrapper } from '@tiptap/react'
+import { README_SECTIONS } from '@/constants'
+import { ActionsBar } from './actions-bar'
+
+export function Commands({ deleteNode }: any) {
+  return (
+    <NodeViewWrapper className='!m-0 !p-0' as='div'>
+      <div className='relative group'>
+        <div className='!outline-none' contentEditable={true} suppressContentEditableWarning={true}>
+          <h2>{README_SECTIONS['commands']}</h2>
+          <p>This extension contributes the following commands to the Command palette:</p>
+          <ul className='list-disc list-outside leading-4 tight' data-tight='true'>
+            <li>
+              <p>
+                <strong>Command name</strong>: Command description.
+              </p>
+            </li>
+            <li>
+              <p>
+                <strong>Authenticate</strong>: Command description.
+              </p>
+            </li>
+          </ul>
+        </div>
+        <ActionsBar removeSection={deleteNode} />
+      </div>
+    </NodeViewWrapper>
+  )
+}
---
+++ src/components/editor/custom-extensions/commands.ts
@@ -0,0 +1,23 @@
+import { Node, mergeAttributes } from '@tiptap/core'
+import { ReactNodeViewRenderer } from '@tiptap/react'
+import { Commands } from '@/components/editor/components/commands'
+
+export default Node.create({
+  name: 'commands',
+  group: 'block',
+  atom: true,
+  draggable: true,
+  parseHTML() {
+    return [
+      {
+        tag: 'Commands'
+      }
+    ]
+  },
+  renderHTML({ HTMLAttributes }) {
+    return ['Commands', mergeAttributes(HTMLAttributes)]
+  },
+  addNodeView() {
+    return ReactNodeViewRenderer(Commands)
+  }
+})
