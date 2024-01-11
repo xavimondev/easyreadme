db226b7709e46810e20c46ba510c63428c17fe1e
Xavi Alfaro <nxb.developer@gmail.com>
2024-01-10 19:28:49 -0500

    add table of contents tiptap component

---
+++ src/components/editor/components/table-contents.tsx
@@ -0,0 +1,38 @@
+import { NodeViewWrapper } from '@tiptap/react'
+import { Section } from '@/types'
+import { README_SECTIONS } from '@/constants'
+import { ActionsBar } from './actions-bar'
+
+export function TableContents({ deleteNode, node }: any) {
+  const { attrs } = node
+  const data = attrs.content
+  return (
+    <NodeViewWrapper as='div'>
+      <div className='relative group'>
+        <div className='!outline-none' contentEditable={true} suppressContentEditableWarning={true}>
+          <h2>{README_SECTIONS['table-contents']}</h2>
+          <ul className='list-disc list-outside leading-4 tight' data-tight='true'>
+            {data.map((section: Section) => {
+              const sectionName = README_SECTIONS[section]
+              return (
+                <li key={section}>
+                  <p>
+                    <a
+                      target='_blank'
+                      rel='noopener noreferrer nofollow'
+                      className='text-blue-500 hover:text-blue-600 dark:text-blue-300 dark:hover:text-blue-400 no-underline hover:underline hover:underline-offset-2 transition-colors cursor-pointer'
+                      href={`#${section}`}
+                    >
+                      {sectionName}
+                    </a>
+                  </p>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+        <ActionsBar removeSection={deleteNode} />
+      </div>
+    </NodeViewWrapper>
+  )
+}
---
+++ src/components/editor/custom-extensions/table-contents.ts
@@ -0,0 +1,42 @@
+import { Node, mergeAttributes } from '@tiptap/core'
+import { ReactNodeViewRenderer } from '@tiptap/react'
+import { TableContents } from '@/components/editor/components/table-contents'
+
+export default Node.create({
+  name: 'tableContents',
+  group: 'block',
+  atom: true,
+  draggable: true,
+  parseHTML() {
+    return [
+      {
+        tag: 'TableContents'
+      }
+    ]
+  },
+  addAttributes() {
+    return {
+      content: {
+        default: []
+      }
+    }
+  },
+  addCommands(): any {
+    return {
+      insertTableContents:
+        ({ content }: { content: string }) =>
+        async ({ editor }: any) => {
+          return editor.commands.insertContent({
+            type: 'tableContents',
+            attrs: { content }
+          })
+        }
+    }
+  },
+  renderHTML({ HTMLAttributes }) {
+    return ['TableContents', mergeAttributes(HTMLAttributes)]
+  },
+  addNodeView() {
+    return ReactNodeViewRenderer(TableContents)
+  }
+})
