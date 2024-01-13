9c14624285541292a6d0b04a4ad29f6452ff6c7b
Xavi Alfaro <nxb.developer@gmail.com>
2024-01-13 14:51:50 -0500

    add ui improvements

--- src/components/editor/components/actions-bar.tsx
+++ src/components/editor/components/actions-bar.tsx
@@ -3,9 +3,9 @@ import { Button } from '@/components/ui/button'
 
 export function ActionsBar({ removeSection }: any) {
   return (
-    <div className='flex gap-2 items-center top-0 right-0 absolute opacity-0 group-hover:opacity-100 transition-opacity duration-200 ease-in-out'>
+    <div className='flex gap-2 items-center top-2 right-2 absolute opacity-0 group-hover:opacity-100 transition-opacity duration-200 ease-in-out'>
       <div
-        className='z-10 drag-handle cursor-grab rounded-sm w-9 h-9 hover:bg-white/5 transition-colors duration-300 ease-in-out'
+        className='z-10 drag-handle cursor-grab rounded-sm w-9 h-9 hover:bg-white/10 transition-colors duration-300 ease-in-out'
         contentEditable='false'
         draggable='true'
         data-drag-handle
