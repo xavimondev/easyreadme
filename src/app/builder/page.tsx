de7aad6177355ea19de1a719f271cf1f9b448084
Xavi Alfaro <nxb.developer@gmail.com>
2024-01-18 12:06:40 -0500

    update header ui

--- src/app/builder/page.tsx
+++ src/app/builder/page.tsx
@@ -1,11 +1,12 @@
 import { BuilderSections } from '@/components/builder-sections'
 import { CounterRemaining } from '@/components/counter-remaining'
 import { CreateTemplateButton } from '@/components/create-template-button'
+import { Header } from '@/components/header'
 
 export default function Builder() {
   return (
     <>
-      <header className='border-b mx-2'>
+      <Header>
         <div className='flex h-12 items-center justify-between'>
           MiLog
           <div className='flex gap-2 items-center w-[300px]'>
@@ -13,7 +14,7 @@ export default function Builder() {
             <CreateTemplateButton />
           </div>
         </div>
-      </header>
+      </Header>
       <BuilderSections />
     </>
   )
