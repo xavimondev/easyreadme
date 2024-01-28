5168dd4d4e6c3833f29033a40840096120f464d5
Xavi Alfaro <nxb.developer@gmail.com>
2024-01-28 15:12:17 -0500

    update list templates ui

--- src/components/list-templates.tsx
+++ src/components/list-templates.tsx
@@ -1,14 +1,12 @@
 'use client'
 
-import Image from 'next/image'
-import Link from 'next/link'
-
 import { NodeName } from '@/types/builder'
 import { Template } from '@/types/readme'
 
 import { LIST_TEMPLATES, README_SECTIONS_DATA } from '@/constants'
 import { cn } from '@/lib/utils'
 import { useBuilder } from '@/store'
+import { Badge } from '@/components/ui/badge'
 import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hover-card'
 
 type TemplateItemProps = {
@@ -16,8 +14,7 @@ type TemplateItemProps = {
   buildReadme: ({ data }: { data: NodeName | NodeName[] }) => Promise<void>
 }
 export function TemplateItem({ template, buildReadme }: TemplateItemProps) {
-  const { srcImage, altImage, nameTemplate, authorTemplate, urlAuthor, sections, description } =
-    template
+  const { nameTemplate, sections, description } = template
   const templateSelected = useBuilder((state) => state.templateSelected)
   const setTemplateSelected = useBuilder((state) => state.setTemplateSelected)
   const isSelected = templateSelected === nameTemplate
@@ -26,7 +23,10 @@ export function TemplateItem({ template, buildReadme }: TemplateItemProps) {
     <HoverCard openDelay={100} closeDelay={0}>
       <HoverCardTrigger asChild>
         <div
-          className='w-full rounded-md overflow-hidden cursor-pointer'
+          className={cn(
+            'w-full rounded-md flex flex-col items-start gap-3 border p-3 text-left transition-all hover:bg-accent cursor-pointer',
+            isSelected && 'bg-muted'
+          )}
           onClick={() => {
             buildReadme({
               data: sections!
@@ -34,44 +34,24 @@ export function TemplateItem({ template, buildReadme }: TemplateItemProps) {
             setTemplateSelected(nameTemplate)
           }}
         >
-          <figure>
-            <Image
-              src={srcImage}
-              alt={altImage}
-              width={500}
-              height={500}
-              className={cn(
-                'w-full h-full rounded-md object-cover border border-gray-300 dark:border-none',
-                {
-                  'border-2 border-purple-600': isSelected
-                }
-              )}
-            />
-            <figcaption
-              className={cn('text-gray-400 text-sm mt-2 text-center italic font-semibold', {
-                'text-purple-500': isSelected
-              })}
-            >
-              {nameTemplate}
-              {authorTemplate && urlAuthor && (
-                <>
-                  by{' '}
-                  <Link href={urlAuthor} className='underline underline-offset-1'>
-                    {authorTemplate}
-                  </Link>
-                </>
-              )}
-            </figcaption>
-          </figure>
+          <div className='flex items-center'>
+            <div className='flex items-center gap-2'>
+              <h2 className='font-semibold'>{nameTemplate}</h2>
+            </div>
+          </div>
+          <div className='line-clamp-2 text-sm text-muted-foreground'>{description}</div>
+          <div className='flex items-center gap-2 text-xs'>
+            <Badge variant='default'>API</Badge>
+            <Badge variant='secondary'>Backend</Badge>
+            <Badge variant='outline'>Backend</Badge>
+          </div>
         </div>
       </HoverCardTrigger>
       <HoverCardContent className='w-80 min-w-[450px] h-full' align='start' side='right'>
         <div className='flex flex-col gap-4'>
-          <h4 className='font-medium leading-none'>{nameTemplate}</h4>
-          <p className='text-sm text-muted-foreground'>{description}</p>
           {sections && sections.length > 0 && (
             <div className='flex flex-col gap-2'>
-              <span className='font-medium'>Sections({sections.length}):</span>
+              <h3 className='font-medium'>Sections</h3>
               <ul className='text-sm list-disc list-inside space-y-1'>
                 {sections.map((section) => {
                   const sectionName = README_SECTIONS_DATA.find((sec) => sec.id === section)
@@ -96,7 +76,7 @@ type ListTemplatesProps = {
 
 export function ListTemplates({ buildReadme }: ListTemplatesProps) {
   return (
-    <div className='grid sm:grid-cols-2 grid-cols-1 gap-x-2 gap-y-5'>
+    <div className='flex flex-col gap-3'>
       {LIST_TEMPLATES.map((template: Template) => (
         <TemplateItem key={template.nameTemplate} template={template} buildReadme={buildReadme} />
       ))}
