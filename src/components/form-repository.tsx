d2e0c18beaef3963787de200d532630843bbc6a2
Xavi Alfaro <nxb.developer@gmail.com>
2024-01-29 19:08:09 -0500

    update form repository ui

--- src/components/form-repository.tsx
+++ src/components/form-repository.tsx
@@ -1,28 +1,39 @@
 'use client'
 
-import { Loader2, Zap } from 'lucide-react'
+import { useEffect, useRef, useState } from 'react'
+import { Command } from 'lucide-react'
 import { toast } from 'sonner'
 
-import { NameTemplate } from '@/types/readme'
-
 import { isValidGitHubRepositoryURL } from '@/utils/github'
-import { RepositoryTemplate } from '@/utils/repository-template'
 import { getRepositoryData } from '@/services/github'
 import { checkRateLimit } from '@/services/rate-limit'
 import { useBuilder } from '@/store'
 import { useRemaining } from '@/hooks/use-remaining'
-import { useTemplates } from '@/hooks/use-templates'
-import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { GitIc } from '@/components/icons'
 
 export function FormRepository() {
-  const { minimal, collaborate, empower, inspire, unleash } = useTemplates()
-  const isGenerating = useBuilder((state) => state.isGenerating)
-  const setIsGenerating = useBuilder((state) => state.setIsGenerating)
+  const [inputValue, setInputValue] = useState('')
+  const inputRef = useRef<HTMLInputElement | null>(null)
   const templateSelected = useBuilder((state) => state.templateSelected)
-  const clearContentTemplate = useBuilder((state) => state.clearContentTemplate)
   const { mutate } = useRemaining()
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault()
+        inputRef.current?.focus()
+      }
+
+      if (e.key === 'Escape') {
+        e.preventDefault()
+        inputRef.current?.blur()
+      }
+    }
+
+    document.addEventListener('keydown', down)
+    return () => document.removeEventListener('keydown', down)
+  }, [])
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -33,72 +44,62 @@ export function FormRepository() {
       return
     }
 
-    setIsGenerating(true)
     const msg = await checkRateLimit()
     if (msg) {
       toast.error(msg)
-      setIsGenerating(false)
       return
     }
 
     const data = await getRepositoryData({ urlRepository })
     if (!data) {
       toast.error('Repository not found. Enter a valid GitHub Repository URL.')
-      setIsGenerating(false)
       return
     }
 
-    const repositoryTemplate = new RepositoryTemplate(data)
-
-    clearContentTemplate()
     mutate()
-
-    const listTemplates: Record<NameTemplate, any> = {
-      Minimal: minimal,
-      Collaborate: collaborate,
-      Empower: empower,
-      Inspire: inspire,
-      Unleash: unleash
-    }
-    await listTemplates[templateSelected]({ repositoryTemplate })
-    setIsGenerating(false)
   }
 
   return (
-    <form className='flex flex-col gap-2' onSubmit={handleSubmit}>
-      <div className='relative flex w-full items-center'>
-        <div className='pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3 text-black/50 dark:text-white/40'>
-          <GitIc className='w-4 h-4' />
+    <div className='flex flex-col z-10 group'>
+      <form className='flex gap-2' onSubmit={handleSubmit}>
+        <div className='relative flex w-[500px] items-center border group-focus-within:border-neutral-600 rounded-md group-focus-within:rounded-b-none bg-neutral-800/60 focus-within:bg-transparent transition-colors duration-200'>
+          <div className='flex items-center pointer-events-none absolute inset-y-0 group-focus-within:left-0 left-[90px] pl-0 group-focus-within:pl-3 text-black/50 dark:text-white/40'>
+            <GitIc className='w-4 h-4' />
+          </div>
+          <Input
+            type='url'
+            autoComplete='off'
+            autoCorrect='off'
+            autoCapitalize='off'
+            required
+            ref={inputRef}
+            onChange={(e) => setInputValue(e.target.value)}
+            name='urlRepository'
+            value={inputValue}
+            className='w-full pl-9 border-none focus-visible:outline-none focus-visible:ring-0 placeholder:text-center text-center group-focus-within:placeholder:text-left group-focus-within:text-left group-focus-within:placeholder:text-white/60 placeholder:text-white/40'
+            placeholder='https://github.com/xavimondev/easyreadme'
+          />
+          {/* {inputValue.length > 0 ? (
+            <span
+              className='group-focus-within:flex items-center absolute inset-y-0 right-0 pr-2 hidden text-black/50 dark:text-white/40 focus:outline-none focus:ring-1 focus:ring-neutral-500'
+              onClick={() => setInputValue('')}
+            >
+              <X className='w-4 h-4' />
+            </span>
+          ) : null} */}
+          <div className='flex gap-1 items-center pointer-events-none absolute inset-y-0 right-0 pr-2 group-focus-within:hidden text-black/50 dark:text-white/40'>
+            <Command className='w-4 h-4' />
+            <span>K</span>
+          </div>
         </div>
-        <Input
-          type='url'
-          autoComplete='off'
-          autoCorrect='off'
-          autoCapitalize='off'
-          required
-          name='urlRepository'
-          className='h-11 pl-9 focus-visible:border-gray-500 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0 placeholder:text-muted-foreground/60'
-          placeholder='https://github.com/xavimondev/readme-creator'
-        />
+      </form>
+      <div className='z-10 w-full border border-t-0 group-focus-within:border-neutral-600 group-focus-within:rounded-b-md bg-black/10 shadow-lg opacity-0 group-focus-within:opacity-100 transition-opacity duration-100'>
+        <ul className='py-1 text-white/50 text-sm'>
+          <li className='px-4 py-2 hover:bg-neutral-800 hover:text-white/80'>Previous Search 1</li>
+          <li className='px-4 py-2 hover:bg-neutral-800 hover:text-white/80'>Previous Search 2</li>
+          <li className='px-4 py-2 hover:bg-neutral-800 hover:text-white/80'>Previous Search 3</li>
+        </ul>
       </div>
-      <Button
-        variant='secondary'
-        type='submit'
-        className='flex gap-1 transition-colors border dark:border-gray-800 border-gray-400'
-        disabled={isGenerating}
-      >
-        {!isGenerating ? (
-          <>
-            Generate
-            <Zap className='h-4 w-4' />
-          </>
-        ) : (
-          <>
-            Generating
-            <Loader2 className='animate-spin h-4 w-4' />
-          </>
-        )}
-      </Button>
-    </form>
+    </div>
   )
 }
