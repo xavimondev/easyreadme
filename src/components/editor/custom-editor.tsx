8bf4f96a9ff8575fe1f03e0b56d5639ee44ef020
Xavi Alfaro <nxb.developer@gmail.com>
2024-02-07 23:30:59 -0500

    add custom nodes into a new file

--- src/components/editor/custom-editor.tsx
+++ src/components/editor/custom-editor.tsx
@@ -4,25 +4,7 @@ import { EditorContent, useEditor } from '@tiptap/react'
 
 import { useBuilder } from '@/store'
 import { DEFAULT_EXTENSIONS } from '@/components/editor/extensions'
-import Acknowledgments from '@/components/editor/nodes/acknowledgments'
-import Alert from '@/components/editor/nodes/alert'
-import Badge from '@/components/editor/nodes/badge'
-import Banner from '@/components/editor/nodes/banner'
-import Changelog from '@/components/editor/nodes/changelog'
-import Commands from '@/components/editor/nodes/commands'
-import ContributorsNode from '@/components/editor/nodes/contributors'
-import Deploy from '@/components/editor/nodes/deploy'
-import EnvVariablesGuide from '@/components/editor/nodes/env-variables-guide'
-import Faq from '@/components/editor/nodes/faq'
-import License from '@/components/editor/nodes/license'
-import Overview from '@/components/editor/nodes/overview'
-import Prerequisites from '@/components/editor/nodes/prerequisites'
-import ProjectStructure from '@/components/editor/nodes/project-structure'
-import ProjectSummary from '@/components/editor/nodes/project-summary'
-import Roadmap from '@/components/editor/nodes/roadmap'
-import RunLocally from '@/components/editor/nodes/run-locally'
-import TableContents from '@/components/editor/nodes/table-contents'
-import TechStack from '@/components/editor/nodes/tech-stack'
+import { CUSTOM_NODES } from '@/components/editor/nodes'
 
 export function CustomEditor() {
   const setReadmeEditor = useBuilder((store) => store.setReadmeEditor)
@@ -36,28 +18,7 @@ export function CustomEditor() {
           'prose prose-sm sm:prose-base prose-neutral dark:prose-invert max-w-none font-default focus:outline-none h-[calc(100vh-405px)] md:h-[calc(100vh-106px)] overflow-y-auto scrollbar-hide'
       }
     },
-    extensions: [
-      ...DEFAULT_EXTENSIONS,
-      Roadmap,
-      Banner,
-      Acknowledgments,
-      RunLocally,
-      License,
-      ProjectStructure,
-      Badge,
-      Changelog,
-      Prerequisites,
-      Faq,
-      Commands,
-      Deploy,
-      Overview,
-      TechStack,
-      ProjectSummary,
-      EnvVariablesGuide,
-      TableContents,
-      ContributorsNode,
-      Alert
-    ],
+    extensions: [...DEFAULT_EXTENSIONS, ...CUSTOM_NODES],
     onCreate: ({ editor }) => {
       setReadmeEditor(editor)
     }
---
+++ src/components/editor/nodes.ts
@@ -0,0 +1,41 @@
+import Acknowledgments from '@/components/editor/nodes/acknowledgments'
+import Alert from '@/components/editor/nodes/alert'
+import Badge from '@/components/editor/nodes/badge'
+import Banner from '@/components/editor/nodes/banner'
+import Changelog from '@/components/editor/nodes/changelog'
+import Commands from '@/components/editor/nodes/commands'
+import ContributorsNode from '@/components/editor/nodes/contributors'
+import Deploy from '@/components/editor/nodes/deploy'
+import EnvVariablesGuide from '@/components/editor/nodes/env-variables-guide'
+import Faq from '@/components/editor/nodes/faq'
+import License from '@/components/editor/nodes/license'
+import Overview from '@/components/editor/nodes/overview'
+import Prerequisites from '@/components/editor/nodes/prerequisites'
+import ProjectStructure from '@/components/editor/nodes/project-structure'
+import ProjectSummary from '@/components/editor/nodes/project-summary'
+import Roadmap from '@/components/editor/nodes/roadmap'
+import RunLocally from '@/components/editor/nodes/run-locally'
+import TableContents from '@/components/editor/nodes/table-contents'
+import TechStack from '@/components/editor/nodes/tech-stack'
+
+export const CUSTOM_NODES = [
+  Roadmap,
+  Banner,
+  Acknowledgments,
+  RunLocally,
+  License,
+  ProjectStructure,
+  Badge,
+  Changelog,
+  Prerequisites,
+  Faq,
+  Commands,
+  Deploy,
+  Overview,
+  TechStack,
+  ProjectSummary,
+  EnvVariablesGuide,
+  TableContents,
+  ContributorsNode,
+  Alert
+]
