adb504669706d9ff5d59b9253656b53dcde33f60
Xavi Alfaro <nxb.developer@gmail.com>
2024-02-19 21:05:41 -0500

    add scroll

--- src/components/list-templates.tsx
+++ src/components/list-templates.tsx
@@ -8,6 +8,7 @@ import { cn } from '@/lib/utils'
 import { useBuilder } from '@/store'
 import { useReadme } from '@/hooks/use-readme'
 import { Badge } from '@/components/ui/badge'
+import { ScrollArea } from '@/components/ui/scroll-area'
 
 type TemplateItemProps = {
   template: Template
@@ -49,14 +50,16 @@ export function TemplateItem({ template, buildTemplate }: TemplateItemProps) {
 export function ListTemplates() {
   const { buildTemplate } = useReadme()
   return (
-    <div className='flex flex-col gap-3 px-3.5'>
-      {LIST_TEMPLATES.map((template: Template) => (
-        <TemplateItem
-          key={template.nameTemplate}
-          template={template}
-          buildTemplate={buildTemplate}
-        />
-      ))}
-    </div>
+    <ScrollArea className='md:h-[calc(100vh-128px)]'>
+      <div className='flex flex-col gap-3 px-3.5'>
+        {LIST_TEMPLATES.map((template: Template) => (
+          <TemplateItem
+            key={template.nameTemplate}
+            template={template}
+            buildTemplate={buildTemplate}
+          />
+        ))}
+      </div>
+    </ScrollArea>
   )
 }
