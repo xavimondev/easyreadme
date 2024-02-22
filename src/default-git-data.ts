954d48068026d4cc035405aee7c79b49a9648364
Xavi Alfaro <nxb.developer@gmail.com>
2024-02-21 22:30:33 -0500

    add file to handle default git data

---
+++ src/default-git-data.ts
@@ -0,0 +1,139 @@
+import { BadgeName, NodeName } from '@/types/builder'
+import { GitRepository } from '@/types/git'
+
+export const DEFAULT_REPOSITORY_DATA: GitRepository = {
+  urlRepository: 'https://github.com/xavimondev/easyreadme',
+  repoName: 'easyreadme',
+  owner: 'xavimondev',
+  description:
+    'Easyreadme helps you simplify README creation and generate visually stunning ones with the help of AI and elegant pre-designed templates.',
+  language: 'TypeScript',
+  branch: 'main'
+}
+
+export const DEFAULT_DATA_CACHED = {
+  [NodeName.OVERVIEW]: {
+    content:
+      'Easyreadme streamlines README creation, leveraging AI to craft visually appealing documentation with elegant templates. Simplify your project communication effortlessly.'
+  },
+  [NodeName.LICENSE]: {
+    name: 'MIT',
+    url: 'https://github.com/xavimondev/easyreadme/blob/main/LICENSE'
+  },
+  [NodeName.PROJECT_SUMMARY]: {
+    content: [
+      {
+        name: 'src',
+        link: 'src',
+        description: 'Main source code directory for the TypeScript project.'
+      },
+      {
+        name: 'src/app',
+        link: 'src/app',
+        description: 'Application-specific code and functionalities.'
+      },
+      {
+        name: 'src/components',
+        link: 'src/components',
+        description: 'Reusable UI components for the application.'
+      },
+      {
+        name: 'src/services',
+        link: 'src/services',
+        description: 'Services providing specific functionalities to the application.'
+      },
+      {
+        name: 'src/styles',
+        link: 'src/styles',
+        description: 'Styling configurations and global styles for the project.'
+      },
+      {
+        name: 'public',
+        link: 'public',
+        description: 'Public assets and resources accessible from the application.'
+      },
+      {
+        name: 'public/hero',
+        link: 'public/hero',
+        description: "Hero images or visuals for the project's landing or main page."
+      },
+      {
+        name: 'src/utils',
+        link: 'src/utils',
+        description: 'Utility functions and helper modules for various tasks.'
+      }
+    ]
+  },
+  [NodeName.SETTING_UP]: {
+    content: [
+      {
+        name: 'OPENAI_API_KEY',
+        steps: [
+          'Visit the OpenAI website and sign in to your account.',
+          'Navigate to the API section in your account settings.',
+          'Generate a new API key for access to the GPT models.',
+          'Copy the generated API key and securely store it.'
+        ]
+      },
+      {
+        name: 'GITHUB_ACCESS_TOKEN',
+        steps: [
+          'Log in to your GitHub account.',
+          "Go to 'Settings' and navigate to 'Developer settings'.",
+          "Select 'Personal access tokens' and generate a new token.",
+          "Choose the required scopes for the token, e.g., 'repo' for repository access.",
+          'Copy the generated token and store it securely.'
+        ]
+      },
+      {
+        name: 'KV_REST_API_URL',
+        steps: ['Insert a guide']
+      },
+      {
+        name: 'KV_REST_API_TOKEN',
+        steps: ['Insert a guide']
+      }
+    ]
+  },
+  [NodeName.TECH_STACK]: {
+    content: [
+      {
+        name: '@tiptap/core',
+        link: 'https://www.tiptap.dev/',
+        description: 'WYSIWYG editor framework for Vue.js and React.'
+      },
+      {
+        name: 'swr',
+        link: 'https://swr.vercel.app/',
+        description: 'React Hooks library for data fetching.'
+      },
+      {
+        name: 'zustand',
+        link: 'https://zustand.surge.sh/',
+        description: 'State management for React using Zustand.'
+      },
+      {
+        name: 'tailwindcss',
+        link: 'https://tailwindcss.com/',
+        description: 'A utility-first CSS framework for rapid UI development.'
+      },
+      {
+        name: 'next',
+        link: 'https://nextjs.org/',
+        description: 'React framework for building web applications with server-side rendering.'
+      }
+    ]
+  },
+  [NodeName.RUN_LOCALLY]: {
+    mainLanguage: DEFAULT_REPOSITORY_DATA.language
+  }
+}
+
+export const DEFAULT_BADGES: BadgeName[] = [
+  'stars',
+  'contributors',
+  'top_language',
+  'issues',
+  'deployment',
+  'license'
+]
