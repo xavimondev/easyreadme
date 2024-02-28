24d05b58e4e17bc8e27f3fdd2449d051669b4607
Xavi Alfaro <nxb.developer@gmail.com>
2024-02-27 19:45:38 -0500

    add trailing space validation

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -1,8 +1,9 @@
 export const getSetupCommands = ({ commands }: { commands: string[] }) => {
   let setup = ''
-
-  commands.forEach((command) => {
-    setup += `\n\n${command}`.trim() + `\n\n`
+  const commandsLength = commands.length - 1
+  commands.forEach((command, index) => {
+    const addTrailingSpace = index !== commandsLength
+    setup += `\n\n${command}`.trim() + `${addTrailingSpace ? '\n\n' : ''}`
   })
 
   return setup
