b3e59780a29671659cfdc80e12e6f8e041bda90d
Xavi Alfaro <nxb.developer@gmail.com>
2024-03-05 17:13:23 -0500

    update html to markdown logic

--- src/components/view-code-button.tsx
+++ src/components/view-code-button.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { copyToClipboard } from '@/utils'
+import { addBreaklineBetweenBadges, copyToClipboard, replaceBadgesMarkdownToHtml } from '@/utils'
 import { Code2 } from 'lucide-react'
 import { NodeHtmlMarkdown } from 'node-html-markdown'
 
@@ -14,42 +14,15 @@ export function ViewCodeButton() {
     <Button
       className='p-2'
       onClick={async () => {
-        const html = readmeEditor
-          ?.getJSON()
-          .content?.map((doc) => {
-            const { type } = doc
-            if (type?.includes('custom-')) {
-              return doc.attrs?.html + `\n`
-            } else if (type === 'paragraph') {
-              const content = doc.content
-              let container = ``
-              if (Array.isArray(content)) {
-                container = `<span align='center'>\n`
-                const listBadges = content
-                  .map((con) => {
-                    const {
-                      // @ts-ignore
-                      attrs: { data }
-                    } = con
-                    const { label, url } = data
-                    return `<img src='${url}' alt='${label}' />\n`
-                  })
-                  .join('\n')
-                container += `${listBadges}</span>\n\n`
-              }
-              return container
-            }
-            return ''
-          })
-          .join('\n') as string
-
+        const html = readmeEditor?.getHTML() as string
         const md = NodeHtmlMarkdown.translate(html, {
           bulletMarker: '-',
           textReplace: [[/\\_/g, '_']],
           useInlineLinks: false
         })
-
-        await copyToClipboard(md)
+        const markdown = addBreaklineBetweenBadges({ markdownContent: md })
+        const sanitizeMd = replaceBadgesMarkdownToHtml({ markdownContent: markdown })
+        await copyToClipboard(sanitizeMd)
       }}
     >
       <Code2 className='w-5 h-5 mr-1' />
