012c292ac8355f642ffd13e6a4b4bc496848d7e9
Xavi Alfaro <nxb.developer@gmail.com>
2024-03-16 17:58:21 -0500

    update default data object

--- src/default-git-data.ts
+++ src/default-git-data.ts
@@ -23,104 +23,110 @@ export const DEFAULT_DATA_CACHED = {
     name: 'MIT',
     url: 'https://github.com/xavimondev/easyreadme/blob/main/LICENSE'
   },
-  [NodeName.PROJECT_SUMMARY]: [
-    {
-      name: 'src',
-      link: 'src',
-      description: 'Main source code directory for the TypeScript project.'
-    },
-    {
-      name: 'src/app',
-      link: 'src/app',
-      description: 'Application-specific code and functionalities.'
-    },
-    {
-      name: 'src/components',
-      link: 'src/components',
-      description: 'Reusable UI components for the application.'
-    },
-    {
-      name: 'src/services',
-      link: 'src/services',
-      description: 'Services providing specific functionalities to the application.'
-    },
-    {
-      name: 'src/styles',
-      link: 'src/styles',
-      description: 'Styling configurations and global styles for the project.'
-    },
-    {
-      name: 'public',
-      link: 'public',
-      description: 'Public assets and resources accessible from the application.'
-    },
-    {
-      name: 'public/hero',
-      link: 'public/hero',
-      description: "Hero images or visuals for the project's landing or main page."
-    },
-    {
-      name: 'src/utils',
-      link: 'src/utils',
-      description: 'Utility functions and helper modules for various tasks.'
-    }
-  ],
-  [NodeName.SETTING_UP]: [
-    {
-      name: 'OPENAI_API_KEY',
-      steps: [
-        'Visit the OpenAI website and sign in to your account.',
-        'Navigate to the API section in your account settings.',
-        'Generate a new API key for access to the GPT models.',
-        'Copy the generated API key and securely store it.'
-      ]
-    },
-    {
-      name: 'GITHUB_ACCESS_TOKEN',
-      steps: [
-        'Log in to your GitHub account.',
-        "Go to 'Settings' and navigate to 'Developer settings'.",
-        "Select 'Personal access tokens' and generate a new token.",
-        "Choose the required scopes for the token, e.g., 'repo' for repository access.",
-        'Copy the generated token and store it securely.'
-      ]
-    },
-    {
-      name: 'KV_REST_API_URL',
-      steps: ['Insert a guide.']
-    },
-    {
-      name: 'KV_REST_API_TOKEN',
-      steps: ['Insert a guide.']
-    }
-  ],
-  [NodeName.TECH_STACK]: [
-    {
-      name: '@tiptap/core',
-      link: 'https://www.tiptap.dev/',
-      description: 'WYSIWYG editor framework for Vue.js and React.'
-    },
-    {
-      name: 'swr',
-      link: 'https://swr.vercel.app/',
-      description: 'React Hooks library for data fetching.'
-    },
-    {
-      name: 'zustand',
-      link: 'https://zustand.surge.sh/',
-      description: 'State management for React using Zustand.'
-    },
-    {
-      name: 'tailwindcss',
-      link: 'https://tailwindcss.com/',
-      description: 'A utility-first CSS framework for rapid UI development.'
-    },
-    {
-      name: 'next',
-      link: 'https://nextjs.org/',
-      description: 'React framework for building web applications with server-side rendering.'
-    }
-  ],
+  [NodeName.PROJECT_SUMMARY]: {
+    data: [
+      {
+        name: 'src',
+        link: 'src',
+        description: 'Main source code directory for the TypeScript project.'
+      },
+      {
+        name: 'src/app',
+        link: 'src/app',
+        description: 'Application-specific code and functionalities.'
+      },
+      {
+        name: 'src/components',
+        link: 'src/components',
+        description: 'Reusable UI components for the application.'
+      },
+      {
+        name: 'src/services',
+        link: 'src/services',
+        description: 'Services providing specific functionalities to the application.'
+      },
+      {
+        name: 'src/styles',
+        link: 'src/styles',
+        description: 'Styling configurations and global styles for the project.'
+      },
+      {
+        name: 'public',
+        link: 'public',
+        description: 'Public assets and resources accessible from the application.'
+      },
+      {
+        name: 'public/hero',
+        link: 'public/hero',
+        description: "Hero images or visuals for the project's landing or main page."
+      },
+      {
+        name: 'src/utils',
+        link: 'src/utils',
+        description: 'Utility functions and helper modules for various tasks.'
+      }
+    ]
+  },
+  [NodeName.SETTING_UP]: {
+    data: [
+      {
+        name: 'OPENAI_API_KEY',
+        steps: [
+          'Visit the OpenAI website and sign in to your account.',
+          'Navigate to the API section in your account settings.',
+          'Generate a new API key for access to the GPT models.',
+          'Copy the generated API key and securely store it.'
+        ]
+      },
+      {
+        name: 'GITHUB_ACCESS_TOKEN',
+        steps: [
+          'Log in to your GitHub account.',
+          "Go to 'Settings' and navigate to 'Developer settings'.",
+          "Select 'Personal access tokens' and generate a new token.",
+          "Choose the required scopes for the token, e.g., 'repo' for repository access.",
+          'Copy the generated token and store it securely.'
+        ]
+      },
+      {
+        name: 'KV_REST_API_URL',
+        steps: ['Insert a guide.']
+      },
+      {
+        name: 'KV_REST_API_TOKEN',
+        steps: ['Insert a guide.']
+      }
+    ]
+  },
+  [NodeName.TECH_STACK]: {
+    dependencies: [
+      {
+        name: '@tiptap/core',
+        link: 'https://www.tiptap.dev/',
+        description: 'WYSIWYG editor framework for Vue.js and React.'
+      },
+      {
+        name: 'swr',
+        link: 'https://swr.vercel.app/',
+        description: 'React Hooks library for data fetching.'
+      },
+      {
+        name: 'zustand',
+        link: 'https://zustand.surge.sh/',
+        description: 'State management for React using Zustand.'
+      },
+      {
+        name: 'tailwindcss',
+        link: 'https://tailwindcss.com/',
+        description: 'A utility-first CSS framework for rapid UI development.'
+      },
+      {
+        name: 'next',
+        link: 'https://nextjs.org/',
+        description: 'React framework for building web applications with server-side rendering.'
+      }
+    ]
+  },
   [NodeName.RUN_LOCALLY]: {
     mainLanguage: DEFAULT_REPOSITORY_DATA.language,
     repoName: DEFAULT_REPOSITORY_DATA.repoName,
@@ -136,23 +142,24 @@ export const DEFAULT_DATA_CACHED = {
     runtime: projectPrerequisites?.runtimes?.find((runt) => runt.id === 'Node'),
     typescriptResource: projectPrerequisites?.typescriptResource
   },
-  [NodeName.MONOREPO_SUMMARY]: [
-    {
-      workspace: 'web',
-      description: `Insert a brief description of your workspace.`,
-      paths: [
-        {
-          name: 'ui',
-          description: 'Insert a summary of your this package.'
-        }
-      ]
-    }
-  ],
+  [NodeName.MONOREPO_SUMMARY]: {
+    data: [
+      {
+        workspace: 'web',
+        description: `Insert a brief description of your workspace.`,
+        paths: [
+          {
+            name: 'ui',
+            description: 'Insert a summary of your this package.'
+          }
+        ]
+      }
+    ]
+  },
   [NodeName.CONTRIBUTORS]: {
     repository: DEFAULT_REPOSITORY_DATA.repoName,
     owner: DEFAULT_REPOSITORY_DATA.owner
   },
-  // TODO: add default value
   [NodeName.PROJECT_STRUCTURE]: `
   ├── .env.example
   ├── LICENSE
