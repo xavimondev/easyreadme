db80b01bbce3743e4a69f980dc4922435df44a83
Xavi Alfaro <nxb.developer@gmail.com>
2024-03-17 13:17:39 -0500

    add logo on header

--- src/app/builder/page.tsx
+++ src/app/builder/page.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 import { BuilderSections } from '@/components/builder-sections'
 import { CommandCenter } from '@/components/command-center'
 import { Header } from '@/components/header'
@@ -8,7 +10,13 @@ export default function Builder() {
     <div className='p-2 min-h-screen'>
       <Header>
         <div className='flex flex-col sm:flex-row pb-2 sm:items-center sm:justify-between gap-2 sm:gap-0 w-full'>
-          <span className='hidden sm:block'>LOG</span>
+          <div className='hidden sm:flex items-center cursor-pointer text-white'>
+            <Image src='/extension_icon.png' alt='Application logo' width={30} height={30} />
+            <h1 className='text-lg ml-2 hover:text-violet-200 transition-colors duration-300'>
+              easyreadme
+            </h1>
+          </div>
+
           <CommandCenter />
           <ViewCodeButton />
         </div>
