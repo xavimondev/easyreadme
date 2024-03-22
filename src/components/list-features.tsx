8cb34a370e54348234607b7501e89b055d4290c2
Xavi Alfaro <nxb.developer@gmail.com>
2024-03-21 21:55:20 -0500

    update features

--- src/components/list-features.tsx
+++ src/components/list-features.tsx
@@ -11,7 +11,8 @@ const LIST_FEATURES: Feature[] = [
     title: 'let AI handle the complexity',
     description:
       'Simplify README creation with AI-powered tools. Reduce complexity and enhance productivity effortlessly.',
-    background: 'from-[#f7e397] to-[#c2bb3d] hover:to-[#f0e84e] hover:-rotate-1',
+    background:
+      'from-[#f7e397] to-[#c2bb3d] hover:to-[#f0e84e] hover:-rotate-1 translate-y-6 -translate-x-10 -rotate-[6deg]',
     textColorCard: 'text-yellow-800',
     textColorTitle: 'from-yellow-100 to-yellow-700',
     urlSticker: '/emojis/ai.webp',
@@ -19,10 +20,11 @@ const LIST_FEATURES: Feature[] = [
   },
   {
     id: 'ease',
-    title: 'copy-paste ease',
+    title: 'copy-paste markdown',
     description:
       'Copy and paste with ease. Save time and effort while creating stunning READMEs for your projects.',
-    background: 'from-[#f2c8ec] to-[#e180f4] hover:to-[#e061f9] hover:rotate-1',
+    background:
+      'from-[#f2c8ec] to-[#e180f4] hover:to-[#e061f9] hover:rotate-1 translate-y-10 -translate-x-10 rotate-2',
     textColorCard: 'text-pink-800',
     textColorTitle: 'from-pink-200 to-pink-700',
     urlSticker: '/emojis/clipboard.webp',
@@ -33,7 +35,8 @@ const LIST_FEATURES: Feature[] = [
     title: 'sleek templates',
     description:
       'Choose from a variety of sleek and elegant templates. Enhance readability and organization for your READMEs.',
-    background: 'from-sky-200 to-[#58a6c7] hover:to-[#39819f] hover:-rotate-1',
+    background:
+      'from-sky-200 to-[#58a6c7] hover:to-[#39819f] hover:-rotate-1 translate-y-10 translate-x-2 -rotate-[1deg]',
     textColorCard: 'text-sky-800',
     textColorTitle: 'from-sky-200 to-blue-600',
     urlSticker: '/emojis/template.webp',
@@ -44,7 +47,8 @@ const LIST_FEATURES: Feature[] = [
     title: 'simplified README creation',
     description:
       'Streamline your README creation process with simplified steps. Focus on content while we handle the rest. Increase productivity effortlessly.',
-    background: 'from-[#b8f2d5] to-green-400 hover:to-[#298e4e] hover:rotate-1',
+    background:
+      'from-[#b8f2d5] to-green-400 hover:to-[#298e4e] hover:rotate-1 translate-y-12 -translate-x-6 -rotate-[1deg]',
     textColorCard: 'text-green-800',
     textColorTitle: 'from-green-200 to-teal-700',
     urlSticker: '/emojis/rocket.webp',
@@ -52,10 +56,11 @@ const LIST_FEATURES: Feature[] = [
   },
   {
     id: 'customization',
-    title: 'effortless customization process',
+    title: 'effortless customization',
     description:
       'Customize your README effortlessly. Add or remove sections seamlessly to tailor templates to your project needs.',
-    background: 'from-[#27224d] to-[#151229] hover:to-[#0a071e] hover:-rotate-1',
+    background:
+      'from-[#27224d] to-[#151229] hover:to-[#0a071e] hover:-rotate-1 translate-y-20 -translate-x-72 -rotate-[4deg]',
     textColorCard:
       'text-transparent bg-clip-text bg-gradient-to-r from-indigo-100 via-red-300 to-yellow-100',
     textColorTitle: 'from-indigo-300 via-orange-200 to-red-800',
@@ -63,22 +68,12 @@ const LIST_FEATURES: Feature[] = [
     shadowColor: 'hover:drop-shadow-[0_0_12px_rgba(129,140,248,1)]'
   },
   {
-    id: 'code',
-    title: 'generate markdown effortlessly',
-    description:
-      'Generate Markdown code for your READMEs with ease. Simplify content creation and focus on your project essentials.',
-    background: 'from-[#ae97e4] to-[#d4bece] hover:to-[#be7dad] hover:rotate-1',
-    textColorCard: 'text-purple-900',
-    textColorTitle: 'from-indigo-200 to-purple-800',
-    urlSticker: '/emojis/computer.webp',
-    shadowColor: 'hover:drop-shadow-[0_0_12px_rgba(192,132,252,1)]'
-  },
-  {
     id: 'flexibility',
     title: 'your own api key',
     description:
       'Enter your own API key for flexibility. Seamlessly integrate OpenAI functionalities and customize your experience.',
-    background: 'from-[#ffeda0] to-[#cb6c4a] hover:to-[#ea5a26] hover:-rotate-1',
+    background:
+      'from-[#ffeda0] to-[#cb6c4a] hover:to-[#ea5a26] hover:-rotate-1 -translate-y-5 translate-x-1.5 rotate-3',
     textColorCard: 'text-orange-700',
     textColorTitle: 'from-orange-200 to-amber-700',
     urlSticker: '/emojis/key.webp',
@@ -89,7 +84,8 @@ const LIST_FEATURES: Feature[] = [
     title: 'local execution',
     description:
       'Execute locally with ollama. Enjoy offline capabilities and manage your READMEs without an API key. Simplify your workflow and enhance productivity effortlessly.',
-    background: 'from-[#525a64] to-[#121212] hover:to-[#0b0a0a] hover:rotate-1',
+    background:
+      'from-[#525a64] to-[#121212] hover:to-[#0b0a0a] hover:rotate-1 translate-y-2 translate-x-1.5 rotate-0',
     textColorCard: 'text-zinc-300',
     textColorTitle: 'from-slate-300 to-stone-600',
     urlSticker: '/emojis/antenna.webp',
