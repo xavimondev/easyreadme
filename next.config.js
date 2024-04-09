dafc7a0a8e22e8bb4d7e77b90d58bf2bbb08d893
Xavi Alfaro <nxb.developer@gmail.com>
2024-04-09 17:40:02 -0500

    add git redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        "source": "/git",
+        "destination": "https://github.com/xavimondev/easyreadme",
+        "permanent": true
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
