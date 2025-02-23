581292a10bb800e44b289aee216831ce12856396
Xavi Alfaro <nxb.developer@gmail.com>
2025-02-23 11:40:11 -0500

    add ui small improvements

--- src/components/generation-info.tsx
+++ src/components/generation-info.tsx
@@ -2,7 +2,6 @@
 
 import { KeyRound } from 'lucide-react'
 
-import { RATE_LIMIT } from '@/constants'
 import { useRemaining } from '@/hooks/use-remaining'
 import { Button } from '@/components/ui/button'
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
@@ -11,7 +10,7 @@ import { CounterInfo } from '@/components/counter-info'
 import { FormApiKey } from '@/components/form-api-key'
 
 export function GenerationInfo() {
-  const { data, isLoading } = useRemaining()
+  const { data } = useRemaining()
 
   return (
     <>
@@ -20,7 +19,7 @@ export function GenerationInfo() {
           <Button className='w-full rounded-md p-[1px] focus:outline-none hidden sm:flex gap-2 items-center bg-zinc-900 border hover:bg-zinc-800 transition-colors duration-300'>
             <KeyRound size={18} className='text-purple-300' />
             <span className='text-transparent bg-clip-text bg-gradient-to-tr from-purple-500 via-purple-300 to-purple-100'>
-              {isLoading ? 0 : data.remaining} / {RATE_LIMIT} generations
+              Set your API Key
             </span>
           </Button>
         </PopoverTrigger>
